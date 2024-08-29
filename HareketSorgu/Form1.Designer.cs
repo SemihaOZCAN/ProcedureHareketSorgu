4774aaa226a31c04f3ffcabed9d2f06df0249694
Semiha OZCAN <169284658+SemihaOZCAN@users.noreply.github.com>
2024-08-29 13:47:59 +0300

    ProcedureHareketSorgu

---
+++ HareketSorgu/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace HareketSorgu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dbHareketDataSet = new HareketSorgu.DbHareketDataSet();
+            this.hareketBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.hareketTableAdapter = new HareketSorgu.DbHareketDataSetTableAdapters.HareketTableAdapter();
+            this.ürünAdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.müşteriDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fiyatDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbHareketDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hareketBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ürünAdDataGridViewTextBoxColumn,
+            this.müşteriDataGridViewTextBoxColumn,
+            this.personelDataGridViewTextBoxColumn,
+            this.fiyatDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.hareketBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1330, 457);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dbHareketDataSet
+            // 
+            this.dbHareketDataSet.DataSetName = "DbHareketDataSet";
+            this.dbHareketDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // hareketBindingSource
+            // 
+            this.hareketBindingSource.DataMember = "Hareket";
+            this.hareketBindingSource.DataSource = this.dbHareketDataSet;
+            // 
+            // hareketTableAdapter
+            // 
+            this.hareketTableAdapter.ClearBeforeFill = true;
+            // 
+            // ürünAdDataGridViewTextBoxColumn
+            // 
+            this.ürünAdDataGridViewTextBoxColumn.DataPropertyName = "Ürün Ad";
+            this.ürünAdDataGridViewTextBoxColumn.HeaderText = "Ürün Ad";
+            this.ürünAdDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.ürünAdDataGridViewTextBoxColumn.Name = "ürünAdDataGridViewTextBoxColumn";
+            // 
+            // müşteriDataGridViewTextBoxColumn
+            // 
+            this.müşteriDataGridViewTextBoxColumn.DataPropertyName = "Müşteri";
+            this.müşteriDataGridViewTextBoxColumn.HeaderText = "Müşteri";
+            this.müşteriDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.müşteriDataGridViewTextBoxColumn.Name = "müşteriDataGridViewTextBoxColumn";
+            // 
+            // personelDataGridViewTextBoxColumn
+            // 
+            this.personelDataGridViewTextBoxColumn.DataPropertyName = "Personel";
+            this.personelDataGridViewTextBoxColumn.HeaderText = "Personel";
+            this.personelDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.personelDataGridViewTextBoxColumn.Name = "personelDataGridViewTextBoxColumn";
+            // 
+            // fiyatDataGridViewTextBoxColumn
+            // 
+            this.fiyatDataGridViewTextBoxColumn.DataPropertyName = "Fiyat";
+            this.fiyatDataGridViewTextBoxColumn.HeaderText = "Fiyat";
+            this.fiyatDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.fiyatDataGridViewTextBoxColumn.Name = "fiyatDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1330, 457);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbHareketDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hareketBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DbHareketDataSet dbHareketDataSet;
+        private System.Windows.Forms.BindingSource hareketBindingSource;
+        private DbHareketDataSetTableAdapters.HareketTableAdapter hareketTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ürünAdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn müşteriDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fiyatDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ HareketSorgu/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace HareketSorgu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-KIMUOA0\SQLEXPRESS;Initial Catalog=DbHareket;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dbHareketDataSet.Hareket' table. You can move, or remove it, as needed.
+            this.hareketTableAdapter.Fill(this.dbHareketDataSet.Hareket);
+
+
+
+        }
+    }
+}
